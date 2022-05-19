44320279dabc05cfef417b5957391f45425ca8e2
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-05-19 21:56:01 +0200

    Main - update button

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Kristyna from './foto.Kristyna.jpg';
 import Regina from './foto.Regina.jpg';
-
+import { Link } from 'react-router-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 /* TODO: nastavit na button proklik na např. "O projektu"  nebo po kliknutí může být pop up "Tak klikni na 'O projektu'!" */
 
 export const Main = () => {
   return (
-    <div className="container">
+    <div className="container" id="container">
       <main>
         <h2>
           Ahoj! Vítáme tě na webové stránce věnované informacím ohledně našeho
@@ -23,7 +24,9 @@ export const Main = () => {
           <img className="authors_photo" src={Regina} alt="autorka Regina" />
         </div>
         {/*  button bude mít funkci onClick a dostane nás to na stránku Projekt */}
-        <button className="btn_more"> CHCI SE DOZVĚDĚT ✨ VÍC ✨</button>
+        <Link to="/projekt">
+          <button className="btn_more"> CHCI SE DOZVĚDĚT ✨ VÍC ✨</button>
+        </Link>
       </main>
       <footer>
         <p>Czechitas, Digitální akademie: Web, 2022</p>
