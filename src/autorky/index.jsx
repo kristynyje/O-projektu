a4a29b6575096f70502f799e182542b309f8f4b2
Kristyna Vysoka <kristynavy@gmail.com>
2022-05-19 14:50:21 +0200

    text adjusted

--- src/autorky/index.jsx
+++ src/autorky/index.jsx
@@ -4,7 +4,13 @@ export const Autorky = () => {
   return (
     <>
       <div>
-        <h2> Ahoj, my jsme Regina a Kristýna, autorky projektu ProDobro.</h2>
+        <h2>
+          {' '}
+          Autorky projektu ProDobro jsou Regina a Kristýna. Obě chtěly udělat
+          závěrečný projekt, který bude přínosný pro společnost a ideálně bude
+          pomáhat jakkoliv znevýhodněným lidem. Proto se rozhodly, že vytvoří
+          platformu pro dobročinné aukce a pojmenovaly jí ProDobro.{' '}
+        </h2>
       </div>
     </>
   );
